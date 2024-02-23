96427ae1e6a3237384e060e2138915c80e83783c
ognevnydemon <maksapple2306@gmail.com>
2024-02-23 09:50:25 +0300

    clippy

--- src/algorithm/prime.rs
+++ src/algorithm/prime.rs
@@ -135,18 +135,17 @@ impl Prime for isize {
 /// assert_eq!(sqrtest(455).ok(), Some(PrimeStatus::Composite));
 /// ```
 pub fn sqrtest(num: isize) -> Result<PrimeStatus, PrimeStatusError> {
-    match num {
-        ..=1 => Err(PrimeStatusError),
-        _ => {
-            // FIXME: https://github.com/rust-lang/rust/issues/116226
-            // let sqrt_res = num.isqrt().unsigned_abs()
-            let sqrt_res = (num as f64).sqrt().ceil() as usize;
-            if (3..=sqrt_res).into_par_iter().find_any(|&i| num.unsigned_abs() % i == 0).is_some() {
-                Ok(PrimeStatus::Composite)
-            } else {
-                Ok(PrimeStatus::Prime)
-            }
-        },
+    if let ..=1 = num {
+        Err(PrimeStatusError)
+    } else {
+        // FIXME: https://github.com/rust-lang/rust/issues/116226
+        // let sqrt_res = num.isqrt().unsigned_abs()
+        let sqrt_res = (num as f64).sqrt().ceil() as usize;
+        if (3..=sqrt_res).into_par_iter().find_any(|&i| num.unsigned_abs() % i == 0).is_some() {
+            Ok(PrimeStatus::Composite)
+        } else {
+            Ok(PrimeStatus::Prime)
+        }
     }
 }
 
